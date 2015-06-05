b2068682d6f07179764550d6d297499a9a6d09bb
Per Wiklander <per@wiklanderconsulting.se>
2015-06-05 08:58:29 +0200

    Code cleanup

--- src/main/java/biz/wiklander/tools/IdentifierSentence.java
+++ src/main/java/biz/wiklander/tools/IdentifierSentence.java
@@ -1,107 +1,182 @@
 package biz.wiklander.tools;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class IdentifierSentence {
 
-    // English adjectives
-    private static final List<String> adjectives = Arrays.asList(
-            "cute", "dapper", "large", "small", "long", "short", "thick",
-            "narrow", "deep", "flat", "whole", "low", "high", "near", "far",
-            "fast", "quick", "slow", "early", "late", "bright", "dark",
-            "cloudy", "warm", "cool", "cold", "windy", "noisy", "loud",
-            "quiet", "dry", "clear", "hard", "soft", "heavy", "light",
-            "strong", "weak", "tidy", "clean", "dirty", "empty", "full",
-            "close", "thirsty", "hungry", "fat", "old", "fresh", "dead",
-            "healthy", "sweet", "sour", "bitter", "salty", "good", "bad",
-            "great", "important", "useful", "expensive", "cheap", "free",
-            "difficult", "strong", "weak", "able", "free", "rich", "afraid",
-            "brave", "fine", "sad", "proud", "comfortable", "happy", "clever",
-            "interesting", "famous", "exciting", "funny", "kind", "polite",
-            "fair", "share", "busy", "free", "lazy", "lucky", "careful",
-            "safe", "dangerous", "mad"
-            );
-
-    // English plural nouns (all animals)
-    final private static List<String> nouns = Arrays.asList(
-            "rabbits", "badgers", "foxes", "chickens", "bats", "deer",
-            "snakes", "hares", "hedgehogs", "platypuses", "moles", "mice",
-            "otters", "rats", "squirrels", "stoats", "weasels", "crows",
-            "doves", "ducks", "geese", "hawks", "herons", "kingfishers",
-            "owls", "peafowl", "pheasants", "pigeons", "robins", "rooks",
-            "sparrows", "starlings", "swans", "ants", "bees", "butterflies",
-            "dragonflies", "flies", "moths", "spiders", "pikes", "salmons",
-            "trouts", "frogs", "newts", "toads", "crabs", "lobsters", "clams",
-            "cockles", "mussles", "oysters", "snails", "cattle", "dogs",
-            "donkeys", "goats", "horses", "pigs", "sheep", "ferrets",
-            "gerbils", "guinea pigs", "parrots", "greg", "orcs"
-            );
-
-    // English verbs, past tense
-    private static final List<String> verbs = Arrays.asList(
-            "sang", "played", "knitted", "floundered", "danced", "played",
-            "listened", "ran", "talked", "cuddled", "sat", "kissed", "hugged",
-            "whimpered", "hid", "fought", "whispered", "cried", "snuggled",
-            "walked", "drove", "loitered", "whimpered", "felt", "jumped",
-            "hopped", "went", "married", "engaged", "stomped"
-            );
-
-    // English adverbs
-    private static final List<String> adverbs = Arrays.asList(
-            "jovially", "merrily", "cordially", "easily", "loudly"
-            );
-
-    private static int adjectiveFactor;
-
-    private static int nounFactor;
-
-    private static int verbFactor;
-
-    private static int adverbFactor;
-
-    static {
-        adjectiveFactor = 32;
-        nounFactor = adjectiveFactor * adjectives.size();
-        verbFactor = nounFactor * nouns.size();
-        adverbFactor = verbFactor * verbs.size();
-
+    public static void main(final String[] args) {
+        formatList(SentenceFactory.adverbs, 5);
     }
 
-    public static String random() {
-        int count = randomInt(33) + 2;
+    public static void formatList(final String[] words,
+            final int wordsPerLine) {
 
-        String adjective = randomItem(adjectives);
-        String noun = randomItem(nouns);
-        String verb = randomItem(verbs);
-        String adverb = randomItem(adverbs);
+        Arrays.sort(words);
 
-        return String.format("%s %s %s %s %s", count, adjective, noun, verb,
-                adverb);
+        final int[] charCount = new int[wordsPerLine];
+
+        int i = 0;
+        for (final String word : words) {
+            if (!(charCount[i % wordsPerLine] > word.length())) {
+                charCount[i % wordsPerLine] = word.length();
+            }
+
+            i++;
+        }
+
+        i = 0;
+        for (final String word : words) {
+            if (i % wordsPerLine == 0) {
+                System.out.print("\n");
+            }
+
+            System.out.print(String.format("%1$-"
+                    + (charCount[i % wordsPerLine] + 4)
+                    + "s", "\"" + word + "\", "));
+
+            i++;
+        }
     }
 
-    public static int parse(String sentence) {
-        String[] words = sentence.split(" ");
+    /**
+     * The default 32-bit phrase generator, which creates fun and
+     * easy-to-remember phrases consisting of, in order:
+     * 
+     * <ul>
+     * <li>One of 32 numbers, from 2-33 [5 bits]</li>
+     * <li>One of 128 adjectives [7 bits]</li>
+     * <li>One of 128 subjects (mostly animals, for extra cuteness) [7 bits]</li>
+     * <li>One of 128 verbs [7 bits]</li>
+     * <li>One of 64 adverbs [6 bits]</li>
+     * </ul>
+     * 
+     * <p>
+     * So a random phrase is something like:
+     * 
+     * "23 glossy ants march happily"
+     * </p>
+     * 
+     * <p>
+     * Dividing the phrase into distinct parts of speech with an obvious pattern
+     * and constraints makes memorization easier. Restricting the dictionary
+     * space to as few syllables as possible (in most cases one- or two-syllable
+     * words) makes recitation of the complete phrase simpler. The result is a
+     * phrase that, while taking up more bytes to render digitally,
+     * theoretically takes up significantly less cognitive space than an 8-digit
+     * hex string or even a 6-digit alphanumeric code. This is an experiment. :)
+     * </p>
+     * 
+     * @author Per Wiklander <per@wiklanderconsulting.se>
+     * 
+     *         Original phrases.js by Greg Slovacek <greg@asana.com>
+     *         See also: <a
+     *         href"http://blog.asana.com/2011/09/6-sad-squid-snuggle-softly/">
+     *         The blog post by Greg about the use of this at Asana
+     *         </a>
+     */
+    private static class SentenceFactory {
 
-        int count = Integer.parseInt(words[0], 10);
+        // English plural nouns
+        /* @formatter:off */
+        final private static String[] nouns = new String[] {
+            "ants",        "badgers",     "bats",     "bees",       "butterflies", 
+            "cattle",      "chickens",    "clams",    "cockles",    "crabs",       
+            "crows",       "deer",        "dogs",     "donkeys",    "doves",       
+            "dragonflies", "ducks",       "ferrets",  "flies",      "foxes",       
+            "frogs",       "geese",       "gerbils",  "goats",      "greg",        
+            "guinea pigs", "hares",       "hawks",    "hedgehogs",  "herons",      
+            "horses",      "kingfishers", "lobsters", "mice",       "moles",       
+            "moths",       "mussles",     "newts",    "orcs",       "otters",      
+            "owls",        "oysters",     "parrots",  "peafowl",    "pheasants",   
+            "pigeons",     "pigs",        "pikes",    "platypuses", "rabbits",     
+            "rats",        "robins",      "rooks",    "salmons",    "sheep",       
+            "snails",      "snakes",      "sparrows", "spiders",    "squirrels",   
+            "starlings",   "stoats",      "swans",    "toads",      "trouts",      
+            "weasels",
+        };
+        /* @formatter:on */
 
-        int adjective = adjectives.indexOf(words[1]);
-        int noun = nouns.indexOf(words[2]);
-        int verb = verbs.indexOf(words[3]);
-        int adverb = adverbs.indexOf(words[4]);
+        // English adjectives
+        /* @formatter:off */
+        private static final String[] adjectives = new String[] {
+            "awful",    "bad",      "bashful", "berserk",  "big",       
+            "bizarre",  "black",    "blue",    "boring",   "brawny",    
+            "bright",   "bumpy",    "burly",   "cagey",    "cheerful",  
+            "chilly",   "chubby",   "classy",  "clumsy",   "cold",      
+            "crazy",    "creepy",   "cuddly",  "dashing",  "dirty",     
+            "dizzy",    "drunk",    "dry",     "dull",     "dusty",     
+            "eager",    "evil",     "fancy",   "fast",     "fat",       
+            "faulty",   "fearless", "filthy",  "foamy",    "friendly",  
+            "funny",    "gentle",   "giant",   "glossy",   "good",      
+            "goofy",    "great",    "greedy",  "green",    "groovy",    
+            "grumpy",   "guilty",   "hairy",   "handsome", "happy",     
+            "healthy",  "heavy",    "helpful", "high",     "huge",      
+            "hungry",   "icky",     "itchy",   "jazzy",    "jealous",   
+            "jolly",    "jumpy",    "kind",    "large",    "lazy",      
+            "lean",     "little",   "lively",  "lucky",    "macho",     
+            "magenta",  "magic",    "massive", "meek",     "mighty",    
+            "mindless", "nasty",    "needy",   "new",      "nice",      
+            "noisy",    "odd",      "old",     "orange",   "pretty",    
+            "prickly",  "proud",    "puffy",   "purple",   "quick",     
+            "quiet",    "rabid",    "rebel",   "red",      "righteous", 
+            "round",    "sad",      "sassy",   "scary",    "sedate",    
+            "shallow",  "short",    "silly",   "skillful", "skinny",    
+            "sloppy",   "slow",     "small",   "smelly",   "sneaky",    
+            "snobby",   "strange",  "tacky",   "tall",     "tan",       
+            "tough",    "tricky",   "ugly",    "wicked",   "wise",      
+            "yellow",   "young",    "zany",
+        };
+        /* @formatter:on */
 
-        return count +
-                adjective * adjectiveFactor +
-                noun * nounFactor +
-                verb * verbFactor +
-                adverb * adverbFactor;
-    }
+        // English verbs, past tense
+        /* @formatter:off */
+        private static final String[] verbs = new String[] {
+            "agree",   "applaud", "argue",   "arise",   "arrive", 
+            "attack",  "awaken",  "bake",    "bathe",   "beg",    
+            "behave",  "bite",    "blink",   "blush",   "bounce", 
+            "breathe", "burrow",  "buzz",    "charge",  "chat",   
+            "cheer",   "chew",    "chuckle", "clap",    "cry",    
+            "dance",   "dig",     "dive",    "drink",   "eat",    
+            "feed",    "fight",   "flap",    "flee",    "float",  
+            "fly",     "gather",  "glow",    "grunt",   "hang",   
+            "hide",    "howl",    "hunt",    "itch",    "jog",    
+            "joke",    "jostle",  "jump",    "kick",    "kneel",  
+            "knit",    "krump",   "laugh",   "leap",    "leave",  
+            "look",    "lope",    "march",   "mix",     "moan",   
+            "nuzzle",  "observe", "plan",    "play",    "plead",  
+            "point",   "pray",    "punch",   "push",    "race",   
+            "rejoice", "relax",   "retire",  "return",  "roar",   
+            "rub",     "rumble",  "run",     "rush",    "sail",   
+            "scare",   "scrape",  "scratch", "scream",  "scrub",  
+            "search",  "shake",   "shiver",  "shrink",  "shrug",  
+            "sigh",    "sing",    "sit",     "skip",    "slap",   
+            "sleep",   "slide",   "slip",    "smash",   "snore",  
+            "spar",    "speak",   "spit",    "stand",   "stare",  
+            "step",    "sting",   "stomp",   "stretch", "strike", 
+            "study",   "stumble", "swim",    "talk",    "think",  
+            "tickle",  "travel",  "trot",    "twist",   "wait",   
+            "walk",    "wander",  "watch",   "wave",    "whine",  
+            "whisper", "work",    "wriggle", 
+        };
+        /* @formatter:on */
 
-    private static final String randomItem(List<String> items) {
-        return items.get(randomInt(items.size()));
-    }
-
-    private static final int randomInt(int max) {
-        return (int) Math.floor(Math.random() * max);
+        // English adverbs
+        /* @formatter:off */
+        private static final String[] adverbs = new String[] {
+            "ably",     "always",   "angrily",  "badly",    "blindly", 
+            "boldly",   "bravely",  "briefly",  "brightly", "busily",  
+            "calmly",   "clearly",  "cruelly",  "daily",    "easily",  
+            "equally",  "exactly",  "fast",     "fiercely", "fondly",  
+            "gently",   "gladly",   "greedily", "happily",  "hastily", 
+            "honestly", "hourly",   "hungrily", "joyously", "justly",  
+            "kindly",   "lazily",   "loosely",  "loudly",   "madly",   
+            "merrily",  "monthly",  "neatly",   "often",    "poorly",  
+            "promptly", "quickly",  "quietly",  "rarely",   "roughly", 
+            "rudely",   "sadly",    "safely",   "sharply",  "shyly",   
+            "slowly",   "smoothly", "softly",   "soon",     "sternly", 
+            "swiftly",  "tensely",  "tightly",  "too",      "warmly",  
+            "weakly",   "well",     "wildly",   "yearly",   
+        };
+        /* @formatter:on */
     }
 }
